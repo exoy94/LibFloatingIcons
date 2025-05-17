95afa709690cae0917d52822b5bcbe09723b8594
ExoY94 <exoy@mail.de>
2025-05-17 23:15:44 +0200

    started work on debug commands

--- modules/slashCommands.lua
+++ modules/slashCommands.lua
@@ -9,9 +9,9 @@ local function PrintBool( var, colorCoded, negatory)
   local str = var and "true" or "false" 
   if colorCoded then 
     if negatory then
-      return Util.ColorString(str, var and "red" or "green") 
+      return Util.ColorString(str, var and "red" or "green") --negatory = true --> true is red, false is green
     else 
-      return Util.ColorString(str, var and "green" or "red") 
+      return Util.ColorString(str, var and "green" or "red") --negatory = false --> true is green, false is red
     end
   else 
     return str
@@ -19,73 +19,71 @@ local function PrintBool( var, colorCoded, negatory)
 end
 
 
+local function Divider() 
+  d( Util.ColorString("--------------------------------------------------", "gray") )
+end
+
 --[[ ------------------- ]]
 --[[ -- Chat Command  -- ]]
 --[[ ------------------- ]]
 
 local cmdList = {
-    ["registry"] = "list of the equipped set-pieces for each equipment slot",
-    ["renderlist"] = "prints overview of render-list"
-
+    ["pool"] = "objectPool data summary",
+    ["handler"] = "handler list or specific handler data"
   }
   
- 
-  
+
 SLASH_COMMANDS["/lfi"] = function( input ) 
-    local LFI = LibFloatingIcons.internal
-    local Util = LFI.util
+  local LFI = LibFloatingIcons.internal
+  local Util = LFI.util
+  local CStr = LFI.util.ColorString
 
-    ---deserializ input 
-    input = string.lower(input) 
-    local param = {}
-    for str in string.gmatch(input, "%S+") do
-      table.insert(param, str)
-    end
+  ---deserializ input 
+  --input = string.lower(input) 
+  local param = {}
+  for str in string.gmatch(input, "%S+") do
+    table.insert(param, str)
+  end
+
+  local cmd = string.lower( table.remove(param, 1) )
   
-    local cmd = table.remove(param, 1) 
-    
-    if not cmd or cmd == ""  then 
-      d( zo_strformat("[<<1>>] <<2>>", Util.ColorString("LibFloatingIcons", "cyan"), "command overview") ) 
-      for cmdName, cmdInfo in pairs( cmdList ) do 
-        d( zo_strformat("<<1>> - <<2>>", Util.ColorString(cmdName, "cyan"), cmdInfo) )
-      end
-      d("--------------------")
-    elseif cmd == "renderlist" then 
-      local renderList = {}
-      if param[1] == "position" then 
-        renderList = LFI.positionObjects.renderList 
-      end
-      local num = 0 
-      for _,_ in pairs(renderList) do 
-        num = num + 1 
-      end
-      LFI.debugMsg("Dev", zo_strformat("RenderList for <<1>> with <<2>> objects:", 
-        Util.ColorString(param[1].."Icons", "orange"),
-        Util.ColorString(num, "white") ) )
-      for _,obj in pairs(renderList) do 
-          d( zo_strformat("<<1>> (sn=<<2>>) from <<3>>", obj.objName, obj.sn, obj.handlerName)  )
-      end
-      d( Util.ColorString("--------------------------------------------------", "gray") )
-
-    elseif cmd == "registry" then 
-      local registry = {}
-      if param[1] == "position" then 
-        registry = LFI.positionObjects.registry 
-      end  
-      LFI.debugMsg("Dev", zo_strformat("Registry for <<1>>:", Util.ColorString(param[1].."Icons", "orange")))
-      for zone, subRegistry in pairs(registry) do 
-        local num = 0 
-        for _,_ in pairs(subRegistry) do 
-          num = num + 1 
+  if not cmd or cmd == ""  then 
+    d( zo_strformat("[<<1>>] <<2>>", CStr("LibFloatingIcons", "cyan"), "command overview") ) 
+    for cmdName, cmdInfo in pairs( cmdList ) do 
+      d( zo_strformat("<<1>> - <<2>>", CStr(cmdName, "cyan"), cmdInfo) )
+    end
+    Divider()
+  elseif cmd == "pool" then 
+    d( zo_strformat("[<<1>>] <<2>>", CStr("LibFloatingIcons", "cyan"), "objectPool data") ) 
+    d( zo_strformat("<<1>> counter: <<2>>; stored: <<3>>", CStr("PositionObjects", "orange"), CStr(tostring(LFI.objectPool["position"].objCounter ), "white"), CStr(tostring(#LFI.objectPool["position"].objects ), "white") ) )
+    d( zo_strformat("<<1>> counter: <<2>>; stored: <<3>>", CStr("UnitObjects", "orange"), CStr(tostring(LFI.objectPool["unit"].objCounter ), "white"), CStr(tostring(#LFI.objectPool["unit"].objects ), "white") ) )
+    Divider()
+  elseif cmd == "handler" then 
+    if not param[1] then 
+      -- list of all existing handler and their current obj amounts 
+      d( zo_strformat("[<<1>>] <<2>>", CStr("LibFloatingIcons", "cyan"), "handler list") ) 
+      for name, Interface in pairs(LFI.interfaceVault) do 
+        local numPosObj = 0 
+        for _,_ in pairs(Interface.objectVault.position) do 
+          numPosObj = numPosObj+1
         end
-        d( zo_strformat("SubRegistry for <<1>> (id=<<2>>) with <<3>> objects", 
-        Util.ColorString(GetZoneNameById(zone), "orange"), zone, Util.ColorString(num, "white") ) )
-        for _,obj in pairs(subRegistry) do 
-          d( zo_strformat(".     <<1>> (sn=<<2>>) from <<3>> (enabled=<<4>>,  hidden=<<5>>) ", 
-          Util.ColorString(obj.objName, "orange"), obj.sn, Util.ColorString(obj.handlerName, "white"), 
-          PrintBool(obj.data.enabled, true), PrintBool( obj.controls.rootCtrl:IsHidden(), true, true)  ) )
+        local numUnitObj = 0 
+        for _,_ in pairs(Interface.objectVault.unit) do 
+          numUnitObj = numUnitObj+1
         end
+        d( zo_strformat("<<1>>: positionObj: <<2>>; unitObj: <<3>>", CStr(name, "orange"), CStr(tostring(numPosObj), "white"), CStr(tostring(numUnitObj), "white") ) )
       end
-      d( Util.ColorString("--------------------------------------------------", "gray") )
+    else 
+      if LFI.interfaceVault[ param[1] ] then 
+        d( zo_strformat("[<<1>>] handler <<2>> overview:", CStr("LibFloatingIcons", "cyan"), CStr(param[1], "orange") ) ) 
+        -- objDefaults (position, unit) 
+        -- iconTemplates  
+        -- list of objects 
+        Divider()
+      else 
+        d( zo_strformat("[<<1>>] handler <<2>> does not exist", CStr("LibFloatingIcons", "cyan"), CStr(param[1], "orange") ) ) 
+      end
     end
   end
+end
+
